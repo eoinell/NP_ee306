841ff07c97bf76d1489663ac9500eacd0bfd3d21
eoinell <43847943+eoinell@users.noreply.github.com>
2019-11-12 12:36:15 +0000

    Creation commit

---
+++ mine/examples/github_example.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 15:25:44 2019
+
+Example script for Github demonstration
+
+@author: Eoin Elliott
+"""
+
+
+def example():
+    
+    print "I'm the definitive version"
+
+
+if __name__ == '__main__':
+    example()
